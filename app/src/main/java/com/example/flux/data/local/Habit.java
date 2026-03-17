db11490ff76bdbdf4307e6d42485a0ab780cfd7d
Dyani <dyanhawal@gmail.com>
2026-03-18 00:54:07 +0530

    add habit entity

--- app/src/main/java/com/example/flux/data/local/Habit.java
+++ app/src/main/java/com/example/flux/data/local/Habit.java
@@ -1,4 +1,19 @@
 package com.example.flux.data.local;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "habits")
 public class Habit {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public String name;
+    public String category;
+    public String frequency;
+    public int difficulty;
+    public long createdAt;
+    public boolean isPaused;
+
+
 }
